7c983000f78b35f3e8c58be2fad74d8542e6253e
Ellalimon9 <ella.gorbacova@gmail.com>
2024-04-29 00:36:42 +0300

    s

--- soundscape/sketch.js
+++ soundscape/sketch.js
@@ -1,16 +1,43 @@
 
+    let userClicked = false;
+    
     let labSound;
 
 function preload() {
 
-  labSound = loadSound ("assets/lab.wav");
+  labSound = loadSound ("assets/lab.mp3");
 }
 
     function setup() {
       createCanvas(windowWidth, windowHeight);
-    
+    textSize(50);
+    textAlign(CENTER);
+    textStyle(BOLD);
     }
 
     function draw() {
       background("hotpink");
+
+if (userHasClicked = false) {
+  text("Click to start...",width / 2, height / 2);
+} else {
+  if(labSound.isPlaying() = false){
+    labSound.play();
+      }
+
+let volume = map(
+  mouseX, 
+  0,
+  width,
+  0,
+  1
+)
+
+labSound.setVolume(volume);
+
+     }
+    }
+
+    function mouseClicked() {
+      userHasClicked = true;
     }
