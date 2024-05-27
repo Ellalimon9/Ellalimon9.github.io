f3f8b1e54c90681ef01498c7236b722e1d19ff13
Ellalimon9 <ella.gorbacova@gmail.com>
2024-05-28 00:11:02 +0300

    .

---
+++ p5.spele/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(600, 600);
+    position (center);
+  }
+  
+  function draw() {
+    background(220);
+  }
+
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    ellipse(50,50,80,80);
+  }
