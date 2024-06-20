f935899d075c79a393e48219baa236771b83614f
Ellalimon9 <ella.gorbacova@gmail.com>
2024-06-21 00:19:09 +0300

    da

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,35 +1,67 @@
-let circleX = 100;
-let circleY = 100;
-let diameter = 50;
+let grid;
+const gridSize = 6;
+let squareSize;
+const colors = ['#190019', '#2B124C', '#522B5B', '#854F6C', '#DFB6B2', '#FBE4D8'];
+let xOffset, yOffset;
 
 function setup() {
-
-createCanvas (windowWidth, windowHeight);
-background (0);
+  createCanvas(400, 400);
+  squareSize = width / gridSize;
+  xOffset = (width - gridSize * squareSize) / 2;
+  yOffset = (height - gridSize * squareSize) / 2;
+  initializeGrid();
+  frameRate(2);
 }
 
 function draw() {
+  background(220);
+  drawGrid();
 
-    background(0);
+  if (!isGridFull()) {
+    let x = int(random(gridSize));
+    let y = int(random(gridSize));
+    let colorIndex = int(random(colors.length));
+    let color = colors[colorIndex];
 
-
-fill("white");
-noStroke();
-    circle(mouseX, mouseY, 100);
-
-    stroke("black");
-    noFill();
-    circle(circleX, circleY, 50);
-
-
-    if (mouseX > circleX - 25 && mouseX < circleX + 25) {
-        if (mouseX > circleX - 25 && mouseX < circleX + 25) {
-            
-       circleX = random(0, width);
-       circleY = random(0, height);
-       diameter = diameter - 10;
-        }
-    }
+    grid[x][y] = color; // Directly assign the color, whether it's null or different.
+  } else {
+    noLoop();
+  }
 }
 
+function mousePressed() {
+  initializeGrid();
+  loop();
+}
 
+function initializeGrid() {
+  grid = [];
+  for (let i = 0; i < gridSize; i++) {
+    grid[i] = Array(gridSize).fill(null);
+  }
+}
+
+function drawGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      if (grid[i][j] !== null) {
+        fill(grid[i][j]);
+      } else {
+        fill(255);
+      }
+      stroke(0);
+      rect(xOffset + i * squareSize, yOffset + j * squareSize, squareSize, squareSize);
+    }
+  }
+}
+
+function isGridFull() {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      if (grid[i][j] === null) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
