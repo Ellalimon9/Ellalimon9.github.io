e66c60d763e6342a0627b75ba7544e79a44a2d84
Ellalimon9 <ella.gorbacova@gmail.com>
2024-06-23 13:12:00 +0300

    ю

--- p5/sketch.js
+++ p5/sketch.js
@@ -4,13 +4,15 @@ let squareSize;
 const colors = ['#190019', '#2B124C', '#522B5B', '#854F6C', '#DFB6B2', '#FBE4D8'];
 let xOffset, yOffset;
 
+
 function setup() {
+  createCanvas(windowWidth, windowHeight);
   createCanvas(400, 400);
   squareSize = width / gridSize;
   xOffset = (width - gridSize * squareSize) / 2;
   yOffset = (height - gridSize * squareSize) / 2;
   initializeGrid();
-  frameRate(2);
+  frameRate(10);
 }
 
 function draw() {
